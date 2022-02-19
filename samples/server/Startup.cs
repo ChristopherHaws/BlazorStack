0c56647f97bae665c3496bd01883ec1fb8dbebd1
Christopher Haws <christopher.haws@arvato-scs.com>
2022-02-19 13:21:50 -0800

    Code Cleanup

--- samples/server/Startup.cs
+++ samples/server/Startup.cs
@@ -1,40 +1,38 @@
 using System.Diagnostics;
 using BlazorStyled;
 
-namespace Sample.Server {
-	public class Startup {
-		private readonly IConfiguration configuration;
-		private readonly IWebHostEnvironment env;
+namespace Sample.Server;
 
-		public Startup(IConfiguration configuration, IWebHostEnvironment env) {
-			this.configuration = configuration;
-			this.env = env;
+public class Startup {
+	private readonly IWebHostEnvironment env;
+
+	public Startup(IWebHostEnvironment env) {
+		this.env = env;
+	}
+
+	public void ConfigureServices(IServiceCollection services) {
+		services.AddRazorPages();
+		services.AddServerSideBlazor();
+		services.AddBlazorStyled(this.env.IsDevelopment(), Debugger.IsAttached);
+	}
+
+	public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+		if (env.IsDevelopment()) {
+			app.UseDeveloperExceptionPage();
+		} else {
+			app.UseExceptionHandler("/Error");
+			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+			app.UseHsts();
 		}
 
-		public void ConfigureServices(IServiceCollection services) {
-			services.AddRazorPages();
-			services.AddServerSideBlazor();
-			services.AddBlazorStyled(this.env.IsDevelopment(), Debugger.IsAttached);
-		}
+		app.UseHttpsRedirection();
+		app.UseStaticFiles();
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-			if (env.IsDevelopment()) {
-				app.UseDeveloperExceptionPage();
-			} else {
-				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+		app.UseRouting();
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
-			app.UseEndpoints(endpoints => {
-				endpoints.MapBlazorHub();
-				endpoints.MapFallbackToPage("/_Host");
-			});
-		}
+		app.UseEndpoints(endpoints => {
+			endpoints.MapBlazorHub();
+			endpoints.MapFallbackToPage("/_Host");
+		});
 	}
 }
